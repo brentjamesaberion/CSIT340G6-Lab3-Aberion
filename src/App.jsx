c0cd15a3e90a5b7b066626c17db6bf7edc132f90
brent <brentjamesphz@gmail.com>
2026-09-22 16:36:30 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,78 @@
-function App() {
-  const course = 'Date Structures and Algorithmn'
-  const part1 = 'Linked List'
-  const exercises1 = 10
-  const part2 = 'Sort Array'
-  const exercises2 = 7
-  const part3 = 'Double Linked List'
-  const exercises3 = 14
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part name={props.course1} unit={props.part1} />
+      <Part name={props.course2} unit={props.part2} />
+      <Part name={props.course3} unit={props.part3} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  return <p>{props.name}, Unit: {props.unit}</p>
+}
+
+const Total = (props) => {
+  return (
+    <p>Total unit: {props.total}</p>
+  )
+}
+const Footer = (props) => {
+  return (
+    <footer>
+      {props.name} - {props.courseCode} - {props.section}
+    </footer>
+  )
+}
+
+const App = () => {
+  const course = 'Data Structure and Algorithm'
+
+  const course1 = 'Industry Elective 1'
+  const part1 = 3
+
+  const course2 = 'Data Analytics 1'
+  const part2 = 3
+
+  const course3 = 'Information Management 2'
+  const part3 = 3
+
+
+  const name = 'Brent James Aberion'
+  const courseCode = 'CSIT340'
+  const section = 'G6'
+
+  const total = part1 + part2 + part3
 
   return (
     <>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+
+      <Content
+        course1={course1}
+        part1={part1}
+        course2={course2}
+        part2={part2}
+        course3={course3}
+        part3={part3}
+      />
+
+      <Total total={total} />
+
+      
+      <Footer
+        name={name}
+        courseCode={courseCode}
+        section={section}
+      />
     </>
   )
 }
